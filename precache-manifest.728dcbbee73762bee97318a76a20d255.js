47f30ace17ec9ae5e5f52de1266756e043023ced
HuangDavid <d.huang@wustl.edu>
2019-05-12 21:56:03 -0500

    Updates

---
+++ precache-manifest.728dcbbee73762bee97318a76a20d255.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3112c95328ff8546fc95b30bd99f4d14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f58086ee47c5299c08d",
+    "url": "/static/css/2.4358233e.chunk.css"
+  },
+  {
+    "revision": "b5ab6b59ab01e55d84b2",
+    "url": "/static/css/main.45edc3ef.chunk.css"
+  },
+  {
+    "revision": "6f58086ee47c5299c08d",
+    "url": "/static/js/2.5f8907ae.chunk.js"
+  },
+  {
+    "revision": "b5ab6b59ab01e55d84b2",
+    "url": "/static/js/main.d928991a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e5225faaad9b99c142040d5005080642",
+    "url": "/static/media/nlp-fairytales.e5225faa.png"
+  },
+  {
+    "revision": "36818d4edeb6e006d3abafd7cbe665d6",
+    "url": "/static/media/nyc-gentrification.36818d4e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dh/precache-manifest.fb3934bb4d8df80d2dcf5e951d341ce6.js"
+  "/precache-manifest.728dcbbee73762bee97318a76a20d255.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dh/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
