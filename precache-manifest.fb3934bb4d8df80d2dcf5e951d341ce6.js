f46babcbf043e39d3c0b32daf4a9408537930548
HuangDavid <d.huang@wustl.edu>
2019-05-12 21:51:58 -0500

    Updates

---
+++ precache-manifest.fb3934bb4d8df80d2dcf5e951d341ce6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b5eecfbacf9fe27eef427e15160f7d6",
+    "url": "/dh/index.html"
+  },
+  {
+    "revision": "1458f545d07d4894259e",
+    "url": "/dh/static/css/2.4358233e.chunk.css"
+  },
+  {
+    "revision": "e2f6a744536b58c3eb64",
+    "url": "/dh/static/css/main.45edc3ef.chunk.css"
+  },
+  {
+    "revision": "1458f545d07d4894259e",
+    "url": "/dh/static/js/2.ad8cb037.chunk.js"
+  },
+  {
+    "revision": "e2f6a744536b58c3eb64",
+    "url": "/dh/static/js/main.23c5d458.chunk.js"
+  },
+  {
+    "revision": "6495b1459413b80954ea",
+    "url": "/dh/static/js/runtime~main.86765ed7.js"
+  },
+  {
+    "revision": "e5225faaad9b99c142040d5005080642",
+    "url": "/dh/static/media/nlp-fairytales.e5225faa.png"
+  },
+  {
+    "revision": "36818d4edeb6e006d3abafd7cbe665d6",
+    "url": "/dh/static/media/nyc-gentrification.36818d4e.png"
+  }
+]);
